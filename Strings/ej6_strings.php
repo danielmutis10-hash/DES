260dfc5964c1d6a22fb9df2fa918785de1773762
danielmutis10-hash <daniel.mutis10@gmail.com>
2026-09-25 12:42:51 +0200

    Comienzo ej 6

---
+++ Strings/ej6_strings.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Ejercicio 6</title>
+</head>
+<body>
+    <?php
+
+    $log = "192.168.1.25 - GET /productos/listado.php - 200 - Mozilla/5.0"; 
+
+    ?>
+</body>
+</html>
