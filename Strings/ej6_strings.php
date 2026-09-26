7754ee41ae206681cf64b50cc18d7b5099c88794
danielmutis10-hash <daniel.mutis10@gmail.com>
2026-09-26 16:23:15 +0200

    Cambios ej 6

--- Strings/ej6_strings.php
+++ Strings/ej6_strings.php
@@ -10,6 +10,19 @@
 
     $log = "192.168.1.25 - GET /productos/listado.php - 200 - Mozilla/5.0"; 
 
+    $cachitos = explode("-", $log);
+    $cachititos = explode("/", $cachitos[1]); //Corta en cachitos la parte con metodo y recursos
+    
+
+    $ip = trim($cachitos[0]);
+    $metodo = trim($cachititos[0]);
+    $recurso = sprintf("$cachititos[1]/$cachititos[2]");
+    $http = trim($cachitos[2]);
+    $navegador = trim($cachitos[3]);
+    $tipo = strtoupper(substr($cachititos[2],8,3));
+
+
+
     ?>
 </body>
 </html>
